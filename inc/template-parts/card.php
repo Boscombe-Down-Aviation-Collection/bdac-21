eabb7c80d84aa46226f431294198341f4fc42466
ol-berry01 <info@littlemonsterdigital.co.uk>
2021-05-03 16:36:46 +0100

    Card test branch
    New card development, to be used for displaying events, news and other non-ecommerce post types

---
+++ inc/template-parts/card.php
@@ -0,0 +1,5 @@
+<div class="card">
+    <h3 class="card-title">Something Awesome</h3>
+    <p class="card-body">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci unde numquam laudantium. Porro, laudantium at.</p>
+    <a href="" class="btn">Learn More</a>
+</div>
