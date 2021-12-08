54cd3d2997d7f7e58ae9154b22b35e4b23524934
Oliver Berry <oliver-berry@live.co.uk>
2021-12-08 16:02:51 +0000

    Table with static cotnent
    Update inc/template-parts/blocks/block-tickets.php:
      + on page styles
      + block-tickets-table
      + block-tickets-table-header
      + block-tickets-table-row

--- inc/template-parts/blocks/block-tickets.php
+++ inc/template-parts/blocks/block-tickets.php
@@ -1,16 +1,79 @@
 <?php 
 
     echo '
+        <style>
+            .block-tickets-table {
+                background: #161e4a0C;
+            }
+            .block-tickets-table-header {
+                background: #161e4a;
+            }
+            .block-tickets-table-header h6 {
+                color: #fff;
+                margin: 0;
+            }
+        </style>
+
         <section class="block-tickets">
 
             <div class="container">
                 <div class="row">
 
-                    <h2 class="block-tickets-title">Collection Tickets</h2>
-                    <p class="lead block-tickets-description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta possimus nihil, totam vitae ex provident minus amet veniam sunt veritatis!</p>
+                    <div class="col-12">
+                        <h2 class="block-tickets-title text-left">Tickets for the Collection</h2>
+                        <p class="lead block-tickets-description text-left">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta possimus nihil, totam vitae ex provident minus amet veniam sunt veritatis!</p>
+                    </div>
 
+                
+                </div>
+                
+                <div class="row justify-content-center">
+
+                    <div class="col-12 col-md-8 my-3 my-md-5">
+                    
+                        <div class="row block-tickets-table">
+                        
+                            <div class="col-3 block-tickets-table-header px-2 py-3">
+                                <h6>Ticket Type</h6>
+                            </div>
+                            <div class="col-3 block-tickets-table-header px-2 py-3">
+                                <h6>Adult</h6>
+                            </div>
+                            <div class="col-3 block-tickets-table-header px-2 py-3">
+                                <h6>Child</h6>
+                            </div>
+                            <div class="col-3 block-tickets-table-header px-2 py-3">
+                                <h6>Conc<wbr>ession</h6>
+                            </div>
+                            <div class="col-3 block-tickets-table-row px-2 py-3">
+                                Day Entry
+                            </div>
+                            <div class="col-3 block-tickets-table-row px-2 py-3">
+                                £8.50
+                            </div>
+                            <div class="col-3 block-tickets-table-row px-2 py-3">
+                                £4.50
+                            </div>
+                            <div class="col-3 block-tickets-table-row px-2 py-3">
+                                £6.50
+                            </div>
+                            <div class="col-3 block-tickets-table-row px-2 py-3">
+                                Day Entry
+                            </div>
+                            <div class="col-3 block-tickets-table-row px-2 py-3">
+                                £8.50
+                            </div>
+                            <div class="col-3 block-tickets-table-row px-2 py-3">
+                                £4.50
+                            </div>
+                            <div class="col-3 block-tickets-table-row px-2 py-3">
+                                £6.50
+                            </div>
+    
+                        </div>
+                        
+                    </div>
                 </div>
             </div>
-
         </section>
     ';
