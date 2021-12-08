aca5293ca12d5cf9df11f3e50619b119a02959db
Oliver Berry <oliver-berry@live.co.uk>
2021-12-08 14:44:51 +0000

    Tickets block static header
    Update inc/template-parts/blocks/block-tickets.php:
      + section 
      + container
      + static title
      + static section description
    
    Create page.php:
      + get_header
      + wp loop
      + get footer

---
+++ inc/template-parts/blocks/block-tickets.php
@@ -0,0 +1,16 @@
+<?php 
+
+    echo '
+        <section class="block-tickets">
+
+            <div class="container">
+                <div class="row">
+
+                    <h2 class="block-tickets-title">Collection Tickets</h2>
+                    <p class="lead block-tickets-description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta possimus nihil, totam vitae ex provident minus amet veniam sunt veritatis!</p>
+
+                </div>
+            </div>
+
+        </section>
+    ';
