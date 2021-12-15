72e0cc90e3d2df54380c9535bdc62d38e4148e6c
Oliver Berry <oliver-berry@live.co.uk>
2021-12-15 21:16:14 +0000

    Event single post
    -> inc/scss/main.scss:
      + import event
    
    + inc/scss/post_types/_event.scss:
      + event parent class
      + event hero image and details
    
    -> single-events.php:
      + title variable
      - static test element
      + article container
      + background image
      + image-details (possible rename later) container

--- single-events.php
+++ single-events.php
@@ -2,8 +2,21 @@
 
     get_header();
 
+    // event variables
+    $event_title    = get_the_title();
+
     echo '
-        from single events
+        <article class="event">
+
+            <div class="event-image" style="background: url(' . get_the_post_thumbnail_url() . '); background-size: cover; background-position: center;">
+
+                <div class="event-image-details">
+                    <h2>' . $event_title . '</h2>
+                </div>
+
+            </div>
+
+        </article>
     ';
 
     get_footer();
