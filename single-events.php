4a2f228e3866deab6f2335cf65884c789427587a
Oliver Berry <oliver-berry@live.co.uk>
2021-12-19 14:26:04 +0000

    Event header update
    -> inc/scss/post_types/_event.scss:
      -> image height by 10vh
      + details container box stylings
      - details container bottom rule
      + h2 margin rule
      + p margin and line-height rules
      + presenter rules
    
    -> single-events.php:
      + variables
      + event details markup layout

--- single-events.php
+++ single-events.php
@@ -3,15 +3,28 @@
     get_header();
 
     // event variables
-    $event_title    = get_the_title();
+    $event_title        = get_the_title();
+    $event_speaker      = get_field( 'event_speaker' );
+    $event_date_start   = get_field( 'event_date_start' );
+    $event_time_start   = get_field( 'event_time_start' );
 
     echo '
         <article class="event">
 
             <div class="event-image" style="background: url(' . get_the_post_thumbnail_url() . '); background-size: cover; background-position: center;">
 
-                <div class="event-image-details">
-                    <h2>' . $event_title . '</h2>
+                <div class="row event-image-details">
+
+                    <div class="col-12">
+                        <h2>' . $event_title . '</h2>
+                    </div>
+                    <div class="col-6">
+                        <p>presented by <span class="event-image-details-presenter">' . $event_speaker . '</span></p>
+                    </div>
+                    <div class="col-6">
+                        <p>' . $event_date_start . '</p>
+                        <p>from ' . $event_time_start . '</p>
+                    </div>
                 </div>
 
             </div>
