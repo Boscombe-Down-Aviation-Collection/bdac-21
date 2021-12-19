886769f15d77ff3d71d65b0acfd46379263c7d0b
Oliver Berry <oliver-berry@live.co.uk>
2021-12-19 19:35:09 +0000

    Event header section v1
    -> inc/scss/post_types/_events.scss:
      + responsive &-image rules
      + responsive &-details rules
      + responsive &-meta rules
      -> &-presenter span rules
    
    -> single-events.php:
      -> structure to work better responsively

--- single-events.php
+++ single-events.php
@@ -7,23 +7,34 @@
     $event_speaker      = get_field( 'event_speaker' );
     $event_date_start   = get_field( 'event_date_start' );
     $event_time_start   = get_field( 'event_time_start' );
+    $event_description  = get_field( 'event_description' );
 
     echo '
         <article class="event">
 
             <div class="event-image" style="background: url(' . get_the_post_thumbnail_url() . '); background-size: cover; background-position: center;">
 
-                <div class="row event-image-details">
-
-                    <div class="col-12">
+                <div class="event-image-details">
+                
+                <div class="row">
+                    <div class="col-12 d-flex event-image-details-meta">
                         <h2>' . $event_title . '</h2>
+                        <p> ' . $event_date_start . ' from ' . $event_time_start . '</p>
                     </div>
-                    <div class="col-6">
-                        <p>presented by <span class="event-image-details-presenter">' . $event_speaker . '</span></p>
+                </div>
+                <div class="row">
+                
+                    <div class="col-12 event-image-details-presenter">
+                        <p class="lead">presented by <span>' . $event_speaker . '</span></p>
                     </div>
-                    <div class="col-6">
-                        <p>' . $event_date_start . '</p>
-                        <p>from ' . $event_time_start . '</p>
+                </div>
+                <div class="row d-none">
+                
+                    <div class="col-12">
+                    <button>Attending
+                    </button>
+                    <button>Maybe
+                    </button>
                     </div>
                 </div>
 
