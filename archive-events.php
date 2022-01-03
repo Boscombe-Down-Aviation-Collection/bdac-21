54c4ec5d280e0b0d8ae736f690cf8040613e3707
Oliver Berry <oliver-berry@live.co.uk>
2022-01-03 12:49:47 +0000

    Archive event styling
    -> archive-event.php:
      + post variables
      + bdac-cards in conditional elements
      - col inline style
    
    -> inc/scss/base/_resets.scss:
      -> responsive padding rule
    
    -> inc/scss/components/_card.scss:
      + margin-block and position rules to card
      + &-date style rules

--- archive-events.php
+++ archive-events.php
@@ -14,11 +14,33 @@
                 while( have_posts() ) :
 					the_post();
 
+                    $postId             = $eventsSection->post->ID;
+                    $eventTitle         = get_the_title();
+                    $eventLink          = get_the_permalink();
+                    $eventHeroImg       = get_the_post_thumbnail_url();
+                    $eventSpeaker       = get_field( 'event_speaker', $postId );
+                    $eventDescription   = wp_trim_words( get_field( 'event_description', $postId ), 20 );
+                    $eventDate		    = get_field( 'event_date_start', $postId );
+
                     if ( $i == 0 ) {
 
                         echo '
-                            <div class="col-12 col-md-8" style="background: blue; color: white;">
-                                first
+                            <div class="col-12 col-md-8">
+                                <div class="bdac-card"  style="background: url(' . $eventHeroImg . '); background-size: cover; background-position-x: center; position: relative;">
+                                    <div class="bdac-card-date">
+                                        <p>' . $eventDate . '</p>
+                                    </div>
+                                    <div class="bdac-card-content">
+                                        <h4 class="bdac-card-content-title mb-3">
+                                            <a href="' . $eventLink .'" title="Posts by BDAC Admin" rel="author">' . $eventTitle . '</a>
+                                        </h4>
+                                        <small class="bdac-card-content-meta">Presented by <a href="' . $card_meta_link . '">' . $eventSpeaker . '</a>' . $card_meta . '</small>
+                                        <p class="bdac-card-content-body mt-3">' . $eventDescription . '</p>
+                                        <a href="' . $eventLink .'" class="bdac-card-content-link mt-auto">
+                                            View Event <i class="fas fa-chevron-right" aria-hidden="true"></i>
+                                        </a>
+                                    </div>
+                                </div>
                             </div>
 
                         ';
@@ -26,8 +48,22 @@
                     } else {
 
                         echo '
-                            <div class="col-12 col-md-4" style="background: red; color: white;">
-                                ' . $i . 'Post
+                            <div class="col-12 col-md-4">
+                                <div class="bdac-card"  style="background: url(' . $eventHeroImg . '); background-size: cover; background-position-x: center; position: relative;">
+                                    <div class="bdac-card-date">
+                                        <p>' . $eventDate . '</p>
+                                    </div>
+                                    <div class="bdac-card-content">
+                                        <h4 class="bdac-card-content-title mb-3">
+                                            <a href="' . $eventLink .'" title="Posts by BDAC Admin" rel="author">' . $eventTitle . '</a>
+                                        </h4>
+                                        <small class="bdac-card-content-meta">Presented by <a href="' . $card_meta_link . '">' . $eventSpeaker . '</a>' . $card_meta . '</small>
+                                        <p class="bdac-card-content-body mt-3">' . $eventDescription . '</p>
+                                        <a href="' . $eventLink .'" class="bdac-card-content-link mt-auto">
+                                            View Event <i class="fas fa-chevron-right" aria-hidden="true"></i>
+                                        </a>
+                                    </div>
+                                </div>
                             </div>
                         ';
 
