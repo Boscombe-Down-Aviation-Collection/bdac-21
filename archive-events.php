0a67e76a79baf79073b0d2fc9719d679e703120b
Oliver Berry <oliver-berry@live.co.uk>
2022-01-03 11:48:48 +0000

    Events archive layout
    -> archive-events.php
      + article
      + container
      + row
      + wp loop
      + first and other posts conditional
    
    -> inc/scss/base/_resets.scss:
      -> &-alt margin-top

--- archive-events.php
+++ archive-events.php
@@ -2,7 +2,48 @@
 
     get_header();
 
-    echo 'archive events';
+?>
+
+    <article class="events">
+		<div class="container">
+			<div class="row">
+			
+            <?php 
+            
+                $i = 0;
+                while( have_posts() ) :
+					the_post();
+
+                    if ( $i == 0 ) {
+
+                        echo '
+                            <div class="col-12 col-md-8" style="background: blue; color: white;">
+                                first
+                            </div>
+
+                        ';
+
+                    } else {
+
+                        echo '
+                            <div class="col-12 col-md-4" style="background: red; color: white;">
+                                ' . $i . 'Post
+                            </div>
+                        ';
+
+                    }
+
+                $i++;
+				endwhile;
+				wp_reset_postdata();
+            
+            ?>
+
+			</div><!-- End: Row -->
+		</div><!-- End: Container -->
+	</article>
+
+<?php
 
     get_footer();
 
