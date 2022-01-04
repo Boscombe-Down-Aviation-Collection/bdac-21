e6532cbf643884a58f62c970b854e1c64c89984d
Oliver Berry <oliver-berry@live.co.uk>
2022-01-04 21:59:02 +0000

    Update header
    -> index.php:
      + page-header background
      + page-header-overlay

--- index.php
+++ index.php
@@ -2,11 +2,13 @@
     get_header();
 ?>
 
-    <section class="page-header">
+<section class="page-header d-flex align-items-end" style="background: url(<?php echo get_the_post_thumbnail_url(); ?>); background-size: cover; height: 25vh; position: relative; background-position-y: 40%; padding: 0;">
+        <div class="page-header-overlay" style="background: #061f5a; position: absolute; top: 0; bottom: 0; left: 0; right: 0; opacity: 0.8;
+    "></div>
 		<div class="container">
 			<div class="row">
-				<div class="col p-3">
-					<h1 class="page-banner-title"><?php single_post_title(); ?></h1>
+				<div class="col">
+					<h1 class="page-banner-title bdac-colour-white"><?php single_post_title(); ?></h1>
 					<div class="row justify-content-sm-start justify-content-md-between page-banner-subtitle m-0">
 						<p class="lead bdac-colour-white mb-3 mb-lg-0">Keep up with the latest news and blog posts from the collection</p>
 						<?php 
