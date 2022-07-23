d2091d7b5ec927ecbd7a68e4ef793d3d0cece2af
Oliver Berry <oliver-berry@live.co.uk>
2022-07-23 18:51:49 +0100

    Restructuring header.php
    -> header.php:
      + .fixed-top to header
      -> header-social inside header
      -> set header-social container to display divs left and right

--- header.php
+++ header.php
@@ -14,76 +14,88 @@
 
   <body <?php body_class(); ?>>
     <!-- Header -->
-    <header>
+    <header id="header" class="header fixed-top">
+      <div class="header-social d-none d-md-flex flex-row ">
+          <div class="container d-flex justify-content-between align-items-center">
+            <div class="header-social-opening">
+              <p class="header-social-opening-hours bdac-colour-white m-0">
+                <span class="header-social-opening-hours-current">
+                  <?php 
+                    $today = date('Y M jS');
+                    $args = array(  
+                        'post_type'         => 'opening_hours',
+                        'post_status'       => 'publish',
+                        'posts_per_page'    => 1,
+                        'meta_key'          => 'season_to',
+                        'orderby'           => 'meta_value',
+                        'order'             => 'ASC',
+                        'meta_query'        => array(
+                            array(
+                                'key'       => 'season_to',
+                                'compare'   => '>=',
+                                'value'     => $today
+                            )
+                        )
+                    );
+    
+                    $seasons = new WP_Query( $args ); 
+                        
+                    while ( $seasons->have_posts() ) : $seasons->the_post(); 
+    
+                        $checkDay       = date('w');
+                        $checkHour      = date('ga');
+                        $seasonTitle    = get_the_title();
+                        $seasonStart    = get_field( 'season_from' );
+                        $seasonEnd      = get_field( 'season_to' );
+                        $dateEnd        = get_field( 'season_to' );
+                        $dayOpen        = get_field( 'season_opening' );
+                        $dayClose       = get_field( 'season_closing' );
+                        $dayEntry       = get_field( 'season_entry' );
+                        $weekStart      = get_field( 'season_start' );
+                        $weekEnd        = get_field( 'season_end' );
+    
+                        echo ( 
+                          $checkHour < $dayOpen || $checkHour >= $dayClose ? 
+                          '<strong>Collection Closed</strong>, open ' . ( 
+                            $checkDay == 7 ? 
+                            $weekStart . ' - ' . $weekEnd : ( 
+                              $checkHour > $dayClose ? 
+                              'Today' : 
+                              'Tomorrow'
+                            ) ) . ' ' . $dayOpen . ' - ' . $dayClose : 
+                          '<strong>Collection Open</strong> ' . $dayOpen . ' - ' . $dayClose 
+                        );
+                    endwhile;
+    
+                    wp_reset_postdata(); 
+                  ?>
+                </span>
+                <span class="header-social-opening-hours-weather"></span>
+              </p>
+            </div>
 
-      <nav id="nav" class="navbar <?php echo ( !is_front_page() ? 'navbar-not-front' : ''  ); ?> fixed-top navbar-expand-lg">
-        <div class="header-social d-none d-md-block ">
-          <div class="container">
-          <div class="header-opening">
-            <p class="header-opening-hours bdac-colour-white m-0">
-              <span class="header-opening-hours-current">
-                <?php 
-                  $today = date('Y M jS');
-                  $args = array(  
-                      'post_type'         => 'opening_hours',
-                      'post_status'       => 'publish',
-                      'posts_per_page'    => 1,
-                      'meta_key'          => 'season_to',
-                      'orderby'           => 'meta_value',
-                      'order'             => 'ASC',
-                      'meta_query'        => array(
-                          array(
-                              'key'       => 'season_to',
-                              'compare'   => '>=',
-                              'value'     => $today
-                          )
-                      )
-                  );
-  
-                  $seasons = new WP_Query( $args ); 
-                      
-                  while ( $seasons->have_posts() ) : $seasons->the_post(); 
-  
-                      $checkDay       = date('w');
-                      $checkHour      = date('ga');
-                      $seasonTitle    = get_the_title();
-                      $seasonStart    = get_field( 'season_from' );
-                      $seasonEnd      = get_field( 'season_to' );
-                      $dateEnd        = get_field( 'season_to' );
-                      $dayOpen        = get_field( 'season_opening' );
-                      $dayClose       = get_field( 'season_closing' );
-                      $dayEntry       = get_field( 'season_entry' );
-                      $weekStart      = get_field( 'season_start' );
-                      $weekEnd        = get_field( 'season_end' );
-  
-                      echo ( 
-                        $checkHour < $dayOpen || $checkHour >= $dayClose ? 
-                        '<strong>Collection Closed</strong>, open ' . ( 
-                          $checkDay == 7 ? 
-                          $weekStart . ' - ' . $weekEnd : ( 
-                            $checkHour > $dayClose ? 
-                            'Today' : 
-                            'Tomorrow'
-                          ) ) . ' ' . $dayOpen . ' - ' . $dayClose : 
-                        '<strong>Collection Open</strong> ' . $dayOpen . ' - ' . $dayClose 
-                      );
-                  endwhile;
-  
-                  wp_reset_postdata(); 
-                ?>
-              </span>
-              <span class="header-opening-hours-weather"></span>
-            </p>
-          </div>
-            <div class="header-social-links d-none d-md-block ml-auto">
-              <a href="https://facebook.com/Fastjetcockpits" target="_blank"><i class="fab fa-facebook"></i></a>
-              <a href="https://twitter.com/bdacatoldsarum" target="_blank"><i class="fab fa-twitter"></i></a>
-              <a href="https://instagram.com/bdacatoldsarum" target="_blank"><i class="fab fa-instagram"></i></a>
-              <a href="https://www.youtube.com/channel/UCI9Ana6f0kgVEGPPXzoULBg" target="_blank"><i class="fab fa-youtube"></i></a>
-              <a href="https://tripadvisor.co.uk/Attraction_Review-g186414-d3310055-Reviews-Boscombe_Down_Aviation_Collection-Salisbury_Wiltshire_England.html?m=19905" target="_blank"><i class="fab fa-tripadvisor"></i></a>
+            <div class="header-social-links d-none d-md-block text-right">
+              <a href="https://facebook.com/Fastjetcockpits" target="_blank">
+                <i class="fab fa-facebook"></i>
+              </a>
+              <a href="https://twitter.com/bdacatoldsarum" target="_blank">
+                <i class="fab fa-twitter"></i>
+              </a>
+              <a href="https://instagram.com/bdacatoldsarum" target="_blank">
+                <i class="fab fa-instagram"></i>
+              </a>
+              <a href="https://www.youtube.com/channel/UCI9Ana6f0kgVEGPPXzoULBg" target="_blank">
+                <i class="fab fa-youtube"></i>
+              </a>
+              <a href="https://tripadvisor.co.uk/Attraction_Review-g186414-d3310055-Reviews-Boscombe_Down_Aviation_Collection-Salisbury_Wiltshire_England.html?m=19905" target="_blank">
+                <i class="fab fa-tripadvisor"></i>
+              </a>
             </div>
           </div>
         </div>
+
+        <nav id="nav" class="navbar <?php echo ( !is_front_page() ? 'navbar-not-front' : ''  ); ?> fixed-top navbar-expand-lg">
+        
         <div class="container">
           <a class="navbar-brand logo-container" href="<?php echo esc_url( home_url( '/' ) ); ?>">
             <?php bdacLogo(90); ?>
