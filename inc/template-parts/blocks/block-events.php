e85cdb91986fda71f0137b0654c792332f1ae1b0
Oliver Berry <oliver-berry@live.co.uk>
2022-08-14 20:20:53 +0100

    Dynamic Events Content
    -> inc/scss/base/_variables.scss:
      -> $header-font to Oswald
    
    -> inc/template-parts/blocks/block-events.php:
      + $events_block WP_Query
      - static content

--- inc/template-parts/blocks/block-events.php
+++ inc/template-parts/blocks/block-events.php
@@ -16,56 +16,55 @@
                     <h2>Upcoming Events</h2>
                 </div>
                 
-                <div class="row">
+                <div class="row">';
 
-                    <div class="col col-12 col-md-4">
-                        <div class="bdac-card text-left" style="background: url(http://bdac:8888/wp-content/uploads/lynx-ah7-retirement_xz651_1.jpeg); background-size: cover; background-position-x: center;">
-                            <div class="bdac-card-content">
-                                <h3 class="bdac-card-content-title mb-3">
-                                    <a href="http://bdac:8888/5th-test-post/" title="Posts by BDAC Admin" rel="author">Empire Test Pilot School – Rotary Wing</a>
-                                </h3>
-                                <small class="bdac-card-content-meta">By <a href=""></a><a href="http://bdac:8888/author/bdac_dm1n/" title="Posts by BDAC Admin" rel="author">BDAC Admin</a> in <a href="http://bdac:8888/category/test-category/" rel="category tag">Test Category</a></small>
-                                <p class="bdac-card-content-body mt-3">Nunc augue sapien, imperdiet eu ultrices vitae, malesuada aliquet augue. Morbi vel velit in orci sollicitudin bibendum. Praesent sed massa sodales, pulvinar magna et, pharetra arcu. Donec porta porttitor pretium.…</p>
-                                <a href="http://bdac:8888/5th-test-post/" class="bdac-card-content-link mt-auto">
-                                    Read More <i class="fas fa-chevron-right" aria-hidden="true"></i>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col col-12 col-md-4">
-                        <div class="bdac-card text-left" style="background: url(http://bdac:8888/wp-content/uploads/raf_mountain_rescue-hero.jpeg); background-size: cover; background-position-x: center;">
-                            <div class="bdac-card-content">
-                                <h3 class="bdac-card-content-title mb-3">
-                                    <a href="http://bdac:8888/5th-test-post/" title="Posts by BDAC Admin" rel="author">	
-                                    Mountain Rescue</a>
-                                </h3>
-                                <small class="bdac-card-content-meta">By <a href=""></a><a href="http://bdac:8888/author/bdac_dm1n/" title="Posts by BDAC Admin" rel="author">BDAC Admin</a> in <a href="http://bdac:8888/category/test-category/" rel="category tag">Test Category</a></small>
-                                <p class="bdac-card-content-body mt-3">Nunc augue sapien, imperdiet eu ultrices vitae, malesuada aliquet augue. Morbi vel velit in orci sollicitudin bibendum. Praesent sed massa sodales, pulvinar magna et, pharetra arcu. Donec porta porttitor pretium.…</p>
-                                <a href="http://bdac:8888/5th-test-post/" class="bdac-card-content-link mt-auto">
-                                    Read More <i class="fas fa-chevron-right" aria-hidden="true"></i>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col col-12 col-md-4">
-                        <div class="bdac-card text-left" style="background: url(http://bdac:8888/wp-content/uploads/raf-phantom-opt.jpg); background-size: cover; background-position-x: center;">
-                            <div class="bdac-card-content">
-                                <h3 class="bdac-card-content-title mb-3">
-                                    <a href="http://bdac:8888/5th-test-post/" title="Posts by BDAC Admin" rel="author">I Learned About Flying From That</a>
-                                </h3>
-                                <small class="bdac-card-content-meta">By <a href=""></a><a href="http://bdac:8888/author/bdac_dm1n/" title="Posts by BDAC Admin" rel="author">BDAC Admin</a> in <a href="http://bdac:8888/category/test-category/" rel="category tag">Test Category</a></small>
-                                <p class="bdac-card-content-body mt-3">Nunc augue sapien, imperdiet eu ultrices vitae, malesuada aliquet augue. Morbi vel velit in orci sollicitudin bibendum. Praesent sed massa sodales, pulvinar magna et, pharetra arcu. Donec porta porttitor pretium.…</p>
-                                <a href="http://bdac:8888/5th-test-post/" class="bdac-card-content-link mt-auto">
-                                    Read More <i class="fas fa-chevron-right" aria-hidden="true"></i>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
+                $events_block = new WP_Query( array(
+                    'posts_per_page' => 3,
+                    'post_type' => 'events'
+                ) );
 
+                while( $events_block->have_posts() ) {
+                    $events_block->the_post();
+
+                    $event_image        = get_the_post_thumbnail_url();
+                    $event_title        = get_the_title();
+                    $event_link         = get_the_permalink();
+                    $event_author       = get_the_author_meta('display_name', $author_id);
+                    $eventSpeaker       = get_field( 'event_speaker', $postId );
+                    $event_description  = wp_trim_words( get_field( 'event_description', $postId ), 20 );
+                    $event_date		    = get_field( 'event_date_start', $postId );
+
+                    echo '
+                        <div class="col col-12 col-md-4">
+                            <div class="bdac-card text-left" style="background: url(' . $event_image . '); background-size: cover; background-position-x: center;">' . ( 
+                                $event_date ?
+                                '
+                                    <div class="bdac-card-date">
+                                        <p>' . $event_date . '</p>
+                                    </div>
+                                ' :
+                                ''
+                                ) . '
+                                <div class="bdac-card-content">
+                                    <h3 class="bdac-card-content-title mb-3">
+                                        <a href="' . $event_link . '" title="Posts by BDAC Admin" rel="author">' . $event_title . '</a>
+                                    </h3>
+                                    <small class="bdac-card-content-meta">By <a href=""></a><a href="/" title="Posts by BDAC Admin" rel="author">BDAC Admin</a> in <a href="/" rel="category tag">Test Category</a></small>
+                                    <p class="bdac-card-content-body mt-3">' . $event_description . '</p>
+                                    <a href="' . $event_link . '" class="bdac-card-content-link mt-auto">
+                                        Read More <i class="fas fa-chevron-right" aria-hidden="true"></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    ';
+                }
+                
+            echo '
                 </div>
 
                 <div class="row">
-                    <a href="#">
+                    <a href="/events">
                         <button class="btn-bdac-alt">
                             All Our Events
                         </button>
