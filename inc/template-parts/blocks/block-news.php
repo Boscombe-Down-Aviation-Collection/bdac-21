e5b19cabfc7fb59385344f2d6f9eaef906bfc8fd
Oliver Berry <oliver-berry@live.co.uk>
2022-08-23 22:14:48 +0100

    News block
    -> inc/scss/base/_buttons.scss:
      -> background colour
      -> colour
      -> padding
      -> font
      -> transition
      -> hover
    
    -> inc/scss/layout/block_news.scss:
      + import variables
      + bdac-news style rules
    
    + inc/template-parts/acf-bdac/group_60....json
    
    -> inc/template-parts/blocks/block-news.php:
      + WP_Query loop for 1 news post

--- inc/template-parts/blocks/block-news.php
+++ inc/template-parts/blocks/block-news.php
@@ -1,29 +1,70 @@
-<section class="bdac-news">
+<?php
+    
+    echo '
+        <section class="bdac-news">
+            <div class="container">
+                <div class="row">
+                    <div class="col col-12 col-md-4 p-0 bdac-news-content">
+                        <h2 class="bdac-news-title bdac-bg-white p-3 p-md-5">Recent News</h2>';
 
-    <div class="container">
-        <div class="row">
-            <div class="col col-12 col-md-3 p-0">
-                <h2 class="bdac-news-title bdac-bg-white p-3 p-md-5">Recent News</h2>
-                <div class="bdac-news-article bdac-bg-blue p-3 p-md-5">
-                    <div class="bdac-news-article-title">
-                        <h3 class="bdac-colour-white">Upcoming event title example of longer title</h3>
-                    </div>
-                    <div class="bdac-news-article-content">
-                        <p class="bdac-colour-white">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus aliquet enim et tellus molestie viverra. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus aliquet enim et tellus molestie viverra Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus aliquet enim et tellus molestie viverra.
-                        </p>
-                        <a href="/events">
-                        <button class="btn-bdac-alt">
-                            Read Full Article
-                        </button>
-                    </a>
-                    </div>
+                            $bdac_news = new WP_Query( array(
+                                'posts_per_page' => 1
+                            ) );
+            
+                            while( $bdac_news->have_posts() ) {
+                                $bdac_news->the_post();
+
+                                $news_title     = get_the_title();
+                                $news_excerpt   = wp_trim_words( get_the_excerpt(), 20 );
+                                $news_link      = get_the_permalink();
+                                $news_thumb     = get_the_post_thumbnail_url();
+            
+                                echo '
+                                <div class="bdac-news-article bdac-bg-blue p-3 p-md-5">
+                                    <div class="bdac-news-article-title">
+                                        <h3 class="bdac-colour-white">' . $news_title . '</h3>
+                                    </div>
+                                        <div class="bdac-news-article-content">
+                                            <p class="bdac-colour-white">
+                                                ' . $news_excerpt . '
+                                            </p>
+                                            <a href="' . $news_link . '">
+                                            <button class="btn-bdac-alt">
+                                                Read Full Article
+                                            </button>
+                                        </a>
+                                        </div>
+                                    </div>
+                                </div>
+                                 <div class="col col-12 col-md-8 p-0" style="border-radius: 0.625rem 0.625rem 0.625rem 0; overflow: hidden;">
+                                     <img src="' . $news_thumb . '" class="w-100 h-100" style="object-fit: cover;">
+                                </div>
+                                ';
+                            }
+
+                            // $bdac_news = new WP_Query( 
+                            //     // array(
+                            //     //     'posts_per_page' => 1,
+                            //     //     'post_type' => 'post'
+                            //     // ) 
+                            // );
+
+                            // while( $bdac_news->have_posts() ) {
+                            //     $bdac_news->the_post();
+                        
+                            //         echo '
+                            //             
+                            //         ';
+                            //         wp_reset_postdata();
+
+                echo '
+                    
                 </div>
             </div>
-            <div class="col col-12 col-md-9 p-0" style="border-radius: 0.625rem 0.625rem 0.625rem 0; overflow: hidden;">
-                <img src="http://bdac:8888/wp-content/uploads/a400m-atlas-cockpit.jpg" class="w-100 h-100" style="object-fit: cover;">
-            </div>
-        </div>
-    </div>
+        </section>
+    ';
 
-</section>
+    
+        
+    
+?>
