809ac1c3ffa35614a12f590eec986a76a4c08598
Oliver Berry <oliver-berry@live.co.uk>
2022-08-24 22:04:36 +0100

    News block no-inline
    -> inc/scss/layout/block_news.scss:
      + &-content border radius
      + &-article overflow hidden
      + img object-fit
    
    -> inc/template-parts/blocks/block-news.php:
      - inline styles
      + bdac-news-image
      - commented out code

--- inc/template-parts/blocks/block-news.php
+++ inc/template-parts/blocks/block-news.php
@@ -24,38 +24,23 @@
                                     <div class="bdac-news-article-title">
                                         <h3 class="bdac-colour-white">' . $news_title . '</h3>
                                     </div>
-                                        <div class="bdac-news-article-content">
-                                            <p class="bdac-colour-white">
-                                                ' . $news_excerpt . '
-                                            </p>
-                                            <a href="' . $news_link . '">
-                                            <button class="btn-bdac-alt">
-                                                Read Full Article
-                                            </button>
-                                        </a>
-                                        </div>
+                                    <div class="bdac-news-article-content">
+                                        <p class="bdac-colour-white">
+                                            ' . $news_excerpt . '
+                                        </p>
+                                        <a href="' . $news_link . '">
+                                        <button class="btn-bdac-alt">
+                                            Read Full Article
+                                        </button>
+                                    </a>
                                     </div>
                                 </div>
-                                 <div class="col col-12 col-md-8 p-0" style="border-radius: 0.625rem 0.625rem 0.625rem 0; overflow: hidden;">
-                                     <img src="' . $news_thumb . '" class="w-100 h-100" style="object-fit: cover;">
+                                </div>
+                                <div class="bdac-news-image col col-12 col-md-8 p-0">
+                                     <img src="' . $news_thumb . '" class="w-100 h-100" />
                                 </div>
                                 ';
                             }
-
-                            // $bdac_news = new WP_Query( 
-                            //     // array(
-                            //     //     'posts_per_page' => 1,
-                            //     //     'post_type' => 'post'
-                            //     // ) 
-                            // );
-
-                            // while( $bdac_news->have_posts() ) {
-                            //     $bdac_news->the_post();
-                        
-                            //         echo '
-                            //             
-                            //         ';
-                            //         wp_reset_postdata();
 
                 echo '
                     
