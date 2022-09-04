9e63c7c6070df1568341af73e69474fe7f3d31c7
Oliver Berry <oliver-berry@live.co.uk>
2022-09-04 14:25:01 +0100

    General changes
    -> archive-events.php:
      - page-header
      + container location
      + responsive classes
    
    -> functions.php:
      + bdac-dist-scripts
      - headerMenu register
    
    -> inc/js/scripts.js:
      + Header Background Scroll
    
    -> _buttons.scss:
      -> reduce font-size
      -> reduce line-height
      -> &:hover color and background-color
      - &:hover duplicate
    
    -> _resets.scss:
      + overflow-x hidden on main and main-alt
      + h1, h2, h3. h4, h5, h6 font-weights and text-transform
    
    _titles.scss:
      - font-weight
      - letter-spacing
      + font-weight
    
    -> _cards.scss:
      -> border-radius
    
    -> _header.scss:
      - font-size
    
    -> block_events.scss:
      -> border-radius
    
    -> block_intro.scss:
      + col-md-5 responsive rules
      + col-md-6 responsive rules
      + &-image responsive rules
    
    -> _events.scss:
      + &s responsive rules
      -> container padding
      -> &-next responsive rules
      + &-image responsive rules
      + &-content padding
      + events-intro rules
    
    -> map.php:
      -> comment out old version
      + version based on block_intro

--- archive-events.php
+++ archive-events.php
@@ -2,61 +2,10 @@
 
     get_header();
 
-    echo '
-        <section class="events p-0">
-            <div class="page-header bg-black d-flex align-items-end" style="background-image: url(' . 
-                ( 
-                    $bannerBg ? 
-                    $bannerBg['url'] : 
-                    'http://bdac:8888/wp-content/uploads/TSR-2-Ground-Crew-opt.jpg') . ');">
-                <div class="page-header-overlay" style="background: #d91f26; position: absolute; top: 0; bottom: 0; left: 0; right: 0; opacity: 80%;"></div>
-                <div class="container">
-                    <div class="row">
-                        <div class="col d-flex flex-column">';
-                            cpt_archive_page_title();
-                        echo '
-                            <div class="row justify-content-sm-start justify-content-md-between page-banner-subtitle" style="margin: 0;">' .
-                                (
-                                    function_exists('yoast_breadcrumb') ?
-                                    yoast_breadcrumb( '
-                                        <p id="breadcrumbs" class="bdac-bg-white ml-md-auto mb-0 py-3 px-4" style="display: inline-block;">',
-                                        '</p>' ) :
-                                        '' 
-                                ) . '            
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-    ';
 ?>
-    
 
     <article class="events">
-        <!-- <section class="page-header bg-black d-flex align-items-end" style="background-image: url(' <?php 
-            // echo ( $bannerBg ? $bannerBg['url'] : 'http://bdac:8888/wp-content/uploads/TSR-2-Ground-Crew-opt.jpg'); 
-            ?> ');">
-            <div class="page-header-overlay" style="background: #d91f26; position: absolute; top: 0; bottom: 0; left: 0; right: 0; opacity: 80%;"></div>
-            <div class="container">
-                <div class="row">
-                    <div class="col">
-                        
-                        <?php 
-                            // cpt_archive_page_title();
-                        ?>
-                        <div class="row justify-content-sm-start justify-content-md-between page-banner-subtitle" style="margin: 0;">
-                            <?php 
-                                // if ( function_exists('yoast_breadcrumb') ) {
-                                //     yoast_breadcrumb( '<p id="breadcrumbs" class="bdac-bg-white ml-md-auto mb-0 py-3 px-4" style="display: inline-block;">','</p>' );
-                                // };
-                            ?>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section> -->
-		<div class="container" style="padding: 6.25rem 0">
+		<div class="container">
 			<div class="row justify-content-center">
 			
             <?php 
@@ -76,12 +25,12 @@
                     if ( $i == 0 ) {
 
                         echo '
-                            <div class="col-12 col-md-3">
-                                <h2>Lorem ipsum.</h2>
+                            <div class="events-intro col col-12 col-md-5 me-md-auto">
+                                <h2>BDAC Events & Presentations</h2>
                                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi modi, cupiditate culpa in omnis error voluptatum aspernatur inventore ipsum, nisi delectus sit itaque mollitia similique harum cum earum, vel nam.
                                 </p>
                             </div>
-                            <div class="col-12 col-md-7 event-next">
+                            <div class="col col-12 col-md-6 event-next">
                                 <h3 class="m-0" style="color: #d91f26;">Coming Up Next:</h3>
                                 <div class="bdac-card event-next-image"  style="background: url(' . $eventHeroImg . '); background-size: cover; position: relative;">' . 
                                     ( 
@@ -109,7 +58,7 @@
                     } else {
 
                         echo '
-                            <div class="col-12 col-md-4">
+                            <div class="col col-12 col-md-4 p-md-3">
                                 <div class="bdac-card"  style="background: url(' . $eventHeroImg . '); background-size: cover; background-position-x: center; position: relative;">' . 
                                     ( 
                                         $eventDate ? 
--- inc/js/scripts.js
+++ inc/js/scripts.js
@@ -5,3 +5,19 @@ import Search from "./modules/Search"
 
 /* Instantiate Classes */
 const liveSearch = new Search()
+
+// Header Background Scroll
+window.addEventListener("scroll", () => {
+  const navBar = document.querySelector("#header")
+  const scrollHeight = window.pageYOffset
+  const navHeight = navBar.getBoundingClientRect().height
+
+  function scrollDown() {
+    navBar.classList.add("navbar-scrolled", "bdac-shadow-small")
+  }
+  function scrollUp() {
+    navBar.classList.remove("navbar-scrolled", "bdac-shadow-small")
+  }
+
+  scrollHeight > navHeight ? scrollDown() : scrollUp()
+})
