4657e554d20c9cf7b57d533ebe9bd67941ce1079
Oliver Berry <oliver-berry@live.co.uk>
2022-11-02 22:41:14 +0000

    feature/Tickets update
    -> block_tickets.scss:
      -> border-radius responsive rules
    
    -> block-tickets.php:
      + tickets_content repeater
      + tickets_table repeater

--- inc/template-parts/blocks/block-tickets.php
+++ inc/template-parts/blocks/block-tickets.php
@@ -26,12 +26,16 @@ $tickets_table          = get_field( 'tickets_table' );
                         <h3 class="bdac-colour-white"></h3>
                     </div>
                     <div class="bdac-tickets-content-text">
-                        <p class="bdac-colour-white mb-4 mb-md-3">
-                            Annual tickets available allowing you to visit as many times as you like, please ask in the shop
-                        </p>
-                        <p class="bdac-colour-white mb-4 mb-md-3">
-                            25% reduction for pre-organised groups of 10 or more see the group visit page for more details
-                        </p>
+                        <?php 
+                        if( have_rows('tickets_content') ):
+                            while( have_rows('tickets_content') ) : the_row();
+                                $tickets_text = get_sub_field('tickets_text');
+                                    echo sprintf( '<p class="bdac-colour-white mb-4 mb-md-3">%1$s</p>',
+                                    esc_html( $tickets_text )
+                                );
+                            endwhile;
+                        endif;
+                        ?>
                     </div>
                 </div>
             </div>
@@ -41,24 +45,42 @@ $tickets_table          = get_field( 'tickets_table' );
                     'd-block d-md-none',
                     'Ticket Prices'
                 );
+                if( have_rows('tickets_table') ): ?>
+                    <table width="100%">
+                        <thead class="bdac-tickets-table-head bdac-bg-blue">
+                            <th class="bdac-colour-white px-3 py-2">Ticket Type</th>
+                            <th class="bdac-colour-white px-3 py-2">Summer Prices</th>
+                            <th class="bdac-colour-white px-3 py-2">Winter Prices</th>
+                        </thead>
+                        <tbody>
+                        <?php 
+                        while( have_rows('tickets_table') ) : the_row();
+                            $ticket_type        = get_sub_field('ticket_type');
+                            $ticket_type_cond   = get_sub_field('ticket_type_cond');
+                            $ticket_summer      = get_sub_field('ticket_summer');
+                            $ticket_summer_ga   = get_sub_field('ticket_summer_ga');
+                            $ticket_winter      = get_sub_field('ticket_winter');
+                            $ticket_winter_ga   = get_sub_field('ticket_winter_ga');
+                            echo '
+                                <tr class="bdac-tickets-table-entry">
+                                    <td class="px-3 py-2" width="50%">
+                                        <h6 style="display: inline">' . esc_html( $ticket_type) . '</h6><br><small>(' . wp_kses_post( $ticket_type_cond ) . ')</small>
+                                    </td>
+                                    <td class="px-3 py-2">
+                                        <strong>£' . esc_html( $ticket_summer ) . '</strong><br><small>( £' . esc_html( $ticket_summer_ga ) . ' w/ Gift Aid )</small>
+                                    </td>
+                                    <td class="px-3 py-2">
+                                        <strong>£' . esc_html( $ticket_winter ) . '</strong><br><small>( £' . esc_html( $ticket_winter_ga ) . ' w/ Gift Aid )</small>
+                                    </td>
+                                </tr>';
+                        endwhile;
+                        ?>
+                        </tbody>
+                    </table>
+                <?php
+                endif;
                 ?>
-                <table width="100%">
-                    <thead class="bdac-tickets-table-head bdac-bg-blue">
-                        <th class="bdac-colour-white px-3 py-2" width="50%">Ticket Type</th>
-                        <th class="bdac-colour-white px-3 py-2">Summer Prices</th>
-                        <th class="bdac-colour-white px-3 py-2">Winter Prices</th>
-                        
-                    </thead>
-                    <tbody>
-                        <tr class="bdac-tickets-table-entry">
-                            <td class="px-3 py-2" width="50%"><h6 style="display: inline">Adult</h6> <small>(Under 65)</small></td>
-                            <td class="px-3 py-2">body row 1</td>
-                            <td class="px-3 py-2">body row 1</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-                        
+            </div>      
         </div>
     </div>
 </section>
