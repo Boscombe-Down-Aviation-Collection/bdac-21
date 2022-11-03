0743e7ca1df3368ed31a5507a3fe0f0d1d0bf31d
Oliver Berry <oliver-berry@live.co.uk>
2022-11-03 20:52:15 +0000

    Tickets block/tweaks
    -> block_tickets.scss:
      -> wrapper display flex
      -> th background and border colour
      -> th responsive rules
      - h5 rules
      + hover rules
      + h6 rules
    
    -> block-tickets.php:
      - bdac-bg-blue
      -> refactor variables

--- inc/template-parts/blocks/block-tickets.php
+++ inc/template-parts/blocks/block-tickets.php
@@ -47,21 +47,22 @@ $tickets_table          = get_field( 'tickets_table' );
                 );
                 if( have_rows('tickets_table') ): ?>
                     <table width="100%">
-                        <thead class="bdac-tickets-table-head bdac-bg-blue">
+                        <thead class="bdac-tickets-table-head">
                             <th class="bdac-colour-white px-3 py-2">Ticket Type</th>
                             <th class="bdac-colour-white px-3 py-2">Summer Prices</th>
                             <th class="bdac-colour-white px-3 py-2">Winter Prices</th>
                         </thead>
                         <tbody>
-                        <?php 
-                        while( have_rows('tickets_table') ) : the_row();
-                            $ticket_type        = get_sub_field('ticket_type');
-                            $ticket_type_cond   = get_sub_field('ticket_type_cond');
-                            $ticket_summer      = get_sub_field('ticket_summer');
-                            $ticket_summer_ga   = get_sub_field('ticket_summer_ga');
-                            $ticket_winter      = get_sub_field('ticket_winter');
-                            $ticket_winter_ga   = get_sub_field('ticket_winter_ga');
-                            echo '
+                            <?php 
+                            while( have_rows('tickets_table') ) : the_row();
+                                $ticket_type        = get_sub_field('ticket_type');
+                                $ticket_type_cond   = get_sub_field('ticket_type_cond');
+                                $ticket_summer      = get_sub_field('ticket_summer');
+                                $ticket_summer_ga   = get_sub_field('ticket_summer_ga');
+                                $ticket_winter      = get_sub_field('ticket_winter');
+                                $ticket_winter_ga   = get_sub_field('ticket_winter_ga');
+
+                                echo '
                                 <tr class="bdac-tickets-table-entry">
                                     <td class="px-3 py-2" width="50%">
                                         <h6 style="display: inline">' . esc_html( $ticket_type) . '</h6><br><small>(' . wp_kses_post( $ticket_type_cond ) . ')</small>
@@ -72,9 +73,10 @@ $tickets_table          = get_field( 'tickets_table' );
                                     <td class="px-3 py-2">
                                         <strong>£' . esc_html( $ticket_winter ) . '</strong><br><small>( £' . esc_html( $ticket_winter_ga ) . ' w/ Gift Aid )</small>
                                     </td>
-                                </tr>';
-                        endwhile;
-                        ?>
+                                </tr>
+                                ';
+                            endwhile;
+                            ?>
                         </tbody>
                     </table>
                 <?php
