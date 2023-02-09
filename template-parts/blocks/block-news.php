10bff2cacb2143e5d5523badd7c562dea77191a0
Oliver Berry <oliver-berry@live.co.uk>
2023-02-09 22:57:08 +0000

    Update news block
    -> block-news.php:
      -> refactor markup to use sprintf

--- template-parts/blocks/block-news.php
+++ template-parts/blocks/block-news.php
@@ -1,71 +1,83 @@
 <?php
-
-    $className = 'bdac-news';
-    if ( !empty( $block['className'] ) ) {
-        $className .= ' ' . $block[ 'className' ] ;
-    }
-    if ( !empty( $block['align'] ) ) {
-        $className .= 'align' . $block[ 'align' ] ;
-    }
-
-    
-    echo '
-        <section class="' . $className . '">
-            <div class="container">
-                <div class="row">
-                    <div class="col col-12 col-md-4 p-0 ' . $className . '-content">
-                        <h2 class="' . $className . '-title bdac-bg-white p-3 p-md-5">Recent News</h2>';
-
-                            $bdac_news = new WP_Query( array(
-                                'posts_per_page' => 1
-                            ) );
-            
-                            while( $bdac_news->have_posts() ) {
-                                $bdac_news->the_post();
-
-                                $news_title     = get_the_title();
-                                $news_excerpt   = wp_trim_words( get_the_excerpt(), 20 );
-                                $news_link      = get_the_permalink();
-                                $news_thumb     = get_the_post_thumbnail_url();
-            
-                                echo '
-                                <div class="' . $className . '-article bdac-bg-blue px-3 py-5 p-md-5">
-                                    <div class="' . $className . '-article-title">
-                                        <h3 class="bdac-colour-white">' . $news_title . '</h3>
-                                    </div>
-                                    <div class="' . $className . '-article-content">
-                                        <p class="bdac-colour-white mb-4 mb-md-3">
-                                            ' . $news_excerpt . '
-                                        </p>
-                                        <a href="' . $news_link . '">
-                                        <button class="btn-bdac">
-                                            Read Full Article
-                                        </button>
-                                    </a>
-                                    </div>
-                                </div>
-                                </div>
-                                <div class="' . $className . '-image col col-12 col-md-8 p-0">
-                                     <img src="' . $news_thumb . '" class="w-100 h-100" />
-                                </div>
-                                ';
-                            }
-
-                echo '
-                    
-                </div>
-                <div class="row pt-5 justify-content-center">
-                    <a href="/news">
-                        <button class="btn-bdac-alt">
-                            View All Articles 
-                        </button>
-                    </a>
-                </div>
-            </div>
-        </section>
-    ';
-
-    
-        
-    
+$className = 'bdac-news';
+if ( !empty( $block['className'] ) ) {
+    $className .= ' ' . $block[ 'className' ] ;
+}
+if ( !empty( $block['align'] ) ) {
+    $className .= 'align' . $block[ 'align' ] ;
+}
 ?>
+    
+<section class="<?php echo $className; ?>">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col col-12 col-md-4 p-0 <?php echo $className; ?>-content">
+                <h2 class="<?php echo $className; ?>-title bdac-bg-white p-3 p-md-5">Recent News</h2>
+                    
+                <?php
+                $bdac_news = new WP_Query( array(
+                    'posts_per_page' => 1
+                ) );
+
+                while( $bdac_news->have_posts() ) {
+                    $bdac_news->the_post();
+
+                    $news_title     = get_the_title();
+                    $news_excerpt   = wp_trim_words( get_the_excerpt(), 20 );
+                    $news_link      = get_the_permalink();
+                    $news_thumb     = get_the_post_thumbnail_url();
+
+                ?>
+                    <div class="<?php echo $className; ?>-article bdac-bg-blue px-3 py-5 p-md-5">
+                        <?php  
+                            echo sprintf(
+                                '<div class="-article-title">
+                                    <h3 class="bdac-colour-white">%2$s</h3>
+                                </div>',
+                                $className,
+                                esc_attr( $news_title )
+                            );
+                        ?>
+                        <div class="<?php echo $className; ?>-article-content">
+                            
+                            <?php 
+                            echo sprintf(
+                                '<p class="bdac-colour-white mb-4 mb-md-3">%1$s</p>
+                                <a href="%2$s">
+                                    <button class="btn-bdac">
+                                        Read Full Article
+                                    </button>
+                                </a>',
+                                esc_attr( $news_excerpt ),
+                                $news_link
+                            );
+                            ?>
+                            
+                        </div>
+                    </div>
+                </div>
+            <?php 
+                echo sprintf(
+                    '<div class="%1$s-image col col-12 col-md-6 p-0">
+                        <img src="%2$s" class="w-100 h-100" />
+                    </div>',
+                    $className,
+                    esc_html( $news_thumb )
+                );
+            }
+            ?>
+        </div>
+        <div class="row pt-5 justify-content-center">
+            <?php 
+            echo sprintf(
+                '<a href="%1$s">
+                    <button class="btn-bdac-alt">
+                        View All Articles
+                    </button>
+                </a>',
+                esc_url( get_permalink( get_option( 'page_for_posts' ) ) )
+            );
+            ?>
+        </div>
+    </div>
+</section>
