b727357e97a8201853c89992a52d6bdd6029a847
Oliver Berry <oliver-berry@live.co.uk>
2023-02-26 11:31:47 +0000

    Event updates
    -> style.css:
      -> version
    
    -> block-events.php:
      + $today variable
      -> orderby to meta_value_num
      + meta_query array
    
    -> block-news.php:
      -> sanitisation on $news_title

--- template-parts/blocks/block-events.php
+++ template-parts/blocks/block-events.php
@@ -18,27 +18,36 @@ if ( !empty( $block['align'] ) ) {
         <div class="row">
 
         <?php
-            $events_block = new WP_Query( array(
-                'posts_per_page'    => 3,
-                'post_type'         => 'events',
-                'orderby'           => 'post_date', 
-                'order'             => 'ASC'
-            ) );
+        $today          = date('Ymd');
+        $events_block   = new WP_Query( array(
+            'posts_per_page'    => 3,
+            'post_type'         => 'events',
+            'meta_key'          => 'event_date_start',
+            'orderby'           => 'meta_value_num',
+            'order'             => 'ASC',
+            'meta_query'        => array(
+                array(
+                    'key'       => 'event_date_start',
+                    'compare'   => '>=',
+                    'value'     => $today
+                )
+            )
+        ) );
 
-            while( $events_block->have_posts() ) {
-                $events_block->the_post();
+        while( $events_block->have_posts() ) {
+            $events_block->the_post();
 
-                $event_image        = get_the_post_thumbnail_url();
-                $event_image_id     = get_post_thumbnail_id( get_the_ID() );
-                $event_image_alt    = get_post_meta( $event_image_id, '_wp_attachment_image_alt', true );
-                $event_title        = get_the_title();
-                $event_link         = get_the_permalink();
-                $event_author       = get_the_author_meta( 'display_name' );
-                $event_speaker      = get_field( 'event_speaker', get_the_ID() );
-                $event_description  = wp_trim_words( get_field( 'event_description', get_the_ID() ), 20 );
-                $event_excerpt      = get_the_excerpt();
-                $event_date		    = get_field( 'event_date_start', get_the_ID() );
-                $event_cats         = get_the_category(get_the_ID());
+            $event_image        = get_the_post_thumbnail_url();
+            $event_image_id     = get_post_thumbnail_id( get_the_ID() );
+            $event_image_alt    = get_post_meta( $event_image_id, '_wp_attachment_image_alt', true );
+            $event_title        = get_the_title();
+            $event_link         = get_the_permalink();
+            $event_author       = get_the_author_meta( 'display_name' );
+            $event_speaker      = get_field( 'event_speaker', get_the_ID() );
+            $event_description  = wp_trim_words( get_field( 'event_description', get_the_ID() ), 20 );
+            $event_excerpt      = get_the_excerpt();
+            $event_date		    = get_field( 'event_date_start', get_the_ID() );
+            $event_cats         = get_the_category(get_the_ID());
         ?>
 
                     <div class="col col-12 col-md-4">
